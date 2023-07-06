29f1e0fa0333446c9a5c5a83bb4a8d1bc13d768c
MelihOZKAS <melihozkass@gmail.com>
2023-07-07 02:28:51 +0300

    feat: Şükürler Olsun v3

--- aktivasyon/settings.py
+++ aktivasyon/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL =  "/static/"
-STATIC_ROOT = BASE_DIR /'staticfiles'
+STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = ['basvurular/static']
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR /"media"
