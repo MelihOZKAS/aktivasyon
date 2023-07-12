87431cb5c74221588a23023282a3585c5a206b53
MelihOZKAS <melihozkass@gmail.com>
2023-07-12 22:35:41 +0300

    feat: Şükürler Olsun v6

--- basvurular/context_process.py
+++ basvurular/context_process.py
@@ -1,9 +1,14 @@
 from .models import Bayi_Listesi
+from django.shortcuts import render,redirect,HttpResponse
 
 
 def get_User_Cash(request):
     if request.user.is_authenticated:
-        bayi = Bayi_Listesi.objects.get(user=request.user)
+        try:
+            bayi = Bayi_Listesi.objects.get(user=request.user)
+        except:
+            return redirect('home')
+
         return dict(bayi=bayi)
     else:
         return {}
