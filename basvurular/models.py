1898305cde131dca8342ea344dcff705201d01bd
MelihOZKAS <melihozkass@gmail.com>
2023-08-06 16:09:30 +0300

    feat: dual site Nasip v23

--- basvurular/models.py
+++ basvurular/models.py
@@ -354,10 +354,9 @@ class EvrakPass(models.Model):
     isim = models.CharField(max_length=100)
     soyisim = models.CharField(max_length=100)
     pasaportno = models.CharField(max_length=100)
-    #numara = models.CharField(max_length=100)
+    numara = models.CharField(max_length=100)
     operator = models.CharField(max_length=100)
-    gececegi_operator = models.CharField(max_length=100,null=True,blank=True)
-    simimei = models.CharField(max_length=255, blank=True, null=True)
+    gececegi_operator = models.CharField(max_length=100)
     irtibat = models.CharField(max_length=100)
     adres = models.TextField()
     pass1 = models.ImageField(upload_to=generate_filename, blank=True)
@@ -369,7 +368,6 @@ class EvrakPass(models.Model):
     GizliAciklama = models.TextField(blank=True,null=True)
     tutar = models.DecimalField(max_digits=10, decimal_places=2,blank=True, null=True, default=Decimal('0.00'))
     tarih = models.DateTimeField(auto_now_add=True)
-    #GizliAciklamaOylesine = models.TextField(blank=True,null=True)
 
     def __str__(self):
         return self.isim + ' ' + self.soyisim
